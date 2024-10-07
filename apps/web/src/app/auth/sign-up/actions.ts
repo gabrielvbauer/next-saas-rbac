8c1a012548e9804bd2b341a5abe953d3d9dbe67a
Gabriel Bauer Vieira <gabriel.bauer9@hotmail.com>
2024-10-07 18:43:29 -0300

    commit

---
+++ apps/web/src/app/auth/sign-up/actions.ts
@@ -0,0 +1,76 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { z } from 'zod'
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+const signUpSchema = z
+  .object({
+    name: z.string().refine((value) => value.split(' ').length > 1, {
+      message: 'Please enter your full name',
+    }),
+    email: z
+      .string()
+      .email({ message: 'Pleave provide a valid e-mail address.' }),
+    password: z
+      .string()
+      .min(6, { message: 'Password should have at least 6 characters.' }),
+    password_confirmation: z.string(),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: 'Passsword confirmation does not match',
+    path: ['password_confirmation'],
+  })
+
+export async function signInWithEmailAndPassword(data: FormData) {
+  const result = signUpSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return {
+      success: false,
+      message: null,
+      errors,
+    }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+
+    cookies().set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    console.error()
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: null,
+    errors: null,
+  }
+}
--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -1,53 +1,19 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
-import githubIcon from '@/assets/github-icon.svg'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
+import { useFormState } from '@/hooks/use-form-state'
+
+import { signInWithEmailAndPassword } from '../sign-in/actions'
+import { SignUpForm } from './sign-up-form'
 
 export default function SignUpPage() {
-  return (
-    <form action="" className="space-y-4">
-      <div className="space-y-1">
-        <Label htmlFor="name">Name</Label>
-        <Input name="name" id="name" />
-      </div>
+  const router = useRouter()
 
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email" />
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password">password</Label>
-        <Input name="password" type="password" id="password" />
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password-confirmation">Confirm your password</Label>
-        <Input
-          name="password-confirmation"
-          type="password"
-          id="password-confirmation"
-        />
-      </div>
-
-      <Button type="submit" className="w-full">
-        Create account
-      </Button>
-
-      <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-in">Already registered? Sign in</Link>
-      </Button>
-
-      <Separator />
-
-      <Button type="submit" variant="outline" className="w-full">
-        <Image src={githubIcon} className="mr-2 size-4 dark:invert" alt="" />
-        Sign up with GitHub
-      </Button>
-    </form>
+  const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
+    signInWithEmailAndPassword,
+    () => {
+      router.push('/')
+    },
   )
+
+  return <SignUpForm />
 }
